86892cfa70c96df8261e0ecc758cfd966d1f865c
soloman <61859250@qq.com>
2014-07-13 18:29:23 +0800

    vs2013提交测试

--- CarControl/MainForm.Designer.cs
+++ CarControl/MainForm.Designer.cs
@@ -97,7 +97,7 @@
             this.panel1.Controls.Add(this.lockcar);
             this.panel1.Location = new System.Drawing.Point(6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(383, 136);
+            this.panel1.Size = new System.Drawing.Size(110, 102);
             this.panel1.TabIndex = 4;
             // 
             // unlockcar
